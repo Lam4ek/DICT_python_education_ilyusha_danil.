78cedcb56562e15776ec26e2c285ae83b7b0eda6
Lam4ek <d.i.iljyusha@student.khai.edu>
2021-10-04 23:25:48 +0200

    ChatBot 5-th stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -20,3 +20,23 @@ while i < your_number:
     i = i+1
     print (str(i) + "!")
 print("Completed, have a nice day!")
+print("Let's test your programming knowledge.")
+print("Why do we use methods?")
+first_question = ("1. To repeat a statement multiple times.")
+second_question = ("2. To decompose a program into several small subroutines.")
+third_question = ("3. To determine the execution time of a program.")
+fourth_question = ("4. To interrupt the execution of a program.")
+print(first_question)
+print(second_question)
+print(third_question)
+print(fourth_question)
+h = 0
+while h < 1:
+    answer = int(input())
+    if answer != 2:
+        print("Please, try again.")
+        continue
+    else:
+        print("Completed, have a nice day!")
+        print("Congratulations, have a nice day!")
+        h = h+1
