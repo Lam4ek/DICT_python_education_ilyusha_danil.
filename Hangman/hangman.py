692f82a6f4bd3f7f3baa0bfca46437404bbb2892
Lam4ek <d.i.iljyusha@student.khai.edu>
2021-10-06 16:51:02 +0200

    Hangman 3-rd stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,6 +1,9 @@
 print("HANGMAN")
+list = ['python', 'java', 'javascript', 'php']
+import random
+(random.choice(list))
 a = input(str("Guess the word:"))
-if a=="python":
+if a==(random.choice(list)):
     print("You survived!")
 else:
     print("You lost!")
