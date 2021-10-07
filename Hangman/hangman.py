bcaa25f748bf83d1e403d624d9d16eb19dd5f271
Lam4ek <d.i.iljyusha@student.khai.edu>
2021-10-07 19:32:14 +0200

    Hangman 5-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,13 +1,31 @@
 import random
 print("HANGMAN")
-words = ['python', 'java', 'javascript', 'php']
-b = (random.choice(words))
-c = b[:3] + '-' * (len(b)-3)
-splitted_words = list((random.choice(words)))
-a = input(str("Guess the word" + " " + c + ":"))
-if a==b:
-    print("You survived!")
-else:
-    print("You lost!")
-    print(c)
+wordlist = ['python', 'java', 'javascript', 'php']
+a = random.choice(wordlist)
+guesses = ' '
+attempts = 5
+
+while attempts > 0:
+     missed = 0
+     for letter in a:
+         if letter in guesses:
+             print (letter,end=' ')
+         else:
+           print ('_',end=' ')
+           missed= missed + 1
+
+     print
+
+     if missed == 0:
+         print ('You win!')
+         break
+
+     guess = input('Input a letter: ')
+     guesses += guess
+
+     if guess not in a:
+         attempts = attempts - 1
+         print ("That letter doesn't appear in the word")
+
+
 
