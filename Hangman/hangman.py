aa46ace5ada1da3eb113fb4605f5e6de5b5aeb1e
Lam4ek <d.i.iljyusha@student.khai.edu>
2021-10-19 21:13:01 +0200

    Hangman 6-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -2,30 +2,40 @@ import random
 print("HANGMAN")
 wordlist = ['python', 'java', 'javascript', 'php']
 a = random.choice(wordlist)
-guesses = ' '
-attempts = 5
+guesses = ''
+attempts = 8
+splitted_word = list(a)
 
 while attempts > 0:
-     missed = 0
-     for letter in a:
-         if letter in guesses:
-             print (letter,end=' ')
-         else:
-           print ('_',end=' ')
-           missed= missed + 1
+    missed = 0
+    for letter in a:
+        if letter in guesses:
+            print (letter,end=' ')
+        else:
+            print ('_',end=' ')
+            missed = missed + 1
+    if missed == 0:
+            print ('You win!')
+            break
 
-     print
+    guess = input('Input a letter: ')
 
-     if missed == 0:
-         print ('You win!')
-         break
+    if guess not in a:
+        if guess in guesses:
+            print('No improvements')
+        else:
+            guesses += guess
+            attempts = attempts - 1
+            print("That letter doesn't appear in the word" )
+    else:
+        if guess in guesses:
+            print('No improvements')
+        else:
+            guesses += guess
 
-     guess = input('Input a letter: ')
-     guesses += guess
+    
+      
 
-     if guess not in a:
-         attempts = attempts - 1
-         print ("That letter doesn't appear in the word")
 
 
 
