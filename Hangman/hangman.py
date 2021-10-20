3ba0839750dbc45b9ae09a9f0c68782a07833570
Lam4ek <d.i.iljyusha@student.khai.edu>
2021-10-20 22:17:29 +0200

    Hangman 8-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -6,39 +6,47 @@ a = random.choice(wordlist)
 guesses = ''
 attempts = 2
 splitted_word = list(a)
-
-while attempts > 0:
-    missed = 0
-    for letter in a:
-        if letter in guesses:
-            print (letter,end=' ')
-        else:
-            print ('_',end=' ')
-            missed = missed + 1
-    if missed == 0:
-            print ('You win!')
-            break
-
-    guess = input('Input a letter: ')
-    if len(guess)==1:
-        if guess.islower():
-            if guess not in a:
-                if guess in guesses:
-                    print("You've already guessed this letter.")
+b=input('Type "play" to play the game, "exit" to quit:')
+i = False
+while i == False:
+    if b == 'play':
+        while attempts > 0:
+            missed = 0
+            for letter in a:
+                if letter in guesses:
+                    print (letter,end=' ')
                 else:
-                    guesses += guess
-                    guesses + a
-                    attempts = attempts - 1
-                    print("That letter doesn't appear in the word" )
+                    print ('_',end=' ')
+                    missed = missed + 1
+            if missed == 0:
+                    print ('You win!')
+                    break
+
+            guess = input('Input a letter: ')
+            if len(guess)==1:
+                if guess.islower():
+                    if guess not in a:
+                        if guess in guesses:
+                            print("You've already guessed this letter.")
+                        else:
+                            guesses += guess
+                            guesses + a
+                            attempts = attempts - 1
+                            print("That letter doesn't appear in the word" )
+                    else:
+                        if guess in guesses:
+                            print("You've already guessed this letter.")
+                        else:
+                            guesses += guess
+                else:
+                    print('Please enter a lowercase English letter.')
             else:
-                if guess in guesses:
-                    print("You've already guessed this letter.")
-                else:
-                    guesses += guess
-        else:
-            print('Please enter a lowercase English letter.')
-    else:
-        print("not singer letter")
+                print("not singer letter")
+    if b == 'exit':
+        i = True
+    break
+
+    
             
 
     
