be25517fbdd43263f5443878129afed9b342ef83
Lam4ek <d.i.iljyusha@student.khai.edu>
2021-10-28 21:22:20 +0200

    CoffeeMachine 3-rd stage

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -5,11 +5,24 @@ print("Mixing boiled water with crushed coffee beans")
 print("Pouring coffee into the cup")
 print("Pouring some milk into the cup")
 print("Coffee is ready!")
+water = 200
+milk = 50
+CoffeeBeans = 15
+
+# print("For 25 cups of coffee you will need:")
+# print(str(cups * water) + "ml of water") 
+# print(str(cups * milk) + "ml of milk")
+# print(str(cups * CoffeeBeans) + "g of coffee beans")
+
+amount_of_water = int(input("Write how many ml of water the coffee machine has:"))
+amout_of_milk = int(input("Write how many ml of milk the coffee machine has:"))
+amout_of_CoffeeBeans = int(input("Write how many grams of coffee beans the coffee machine has:"))
 cups = int(input("Write how many cups of coffee you will need:"))
-water = int(200)
-milk = int(50)
-coffee__beans = int(15)
-print("For 25 cups of coffee you will need:")
-print(str(cups * water) + "ml of water") 
-print(str(cups * milk) + "ml of milk")
-print(str(cups * coffee__beans) + "g of coffee beans")
+
+PossibleAmount = min(amount_of_water //  water, amout_of_milk // milk, amout_of_CoffeeBeans // CoffeeBeans)
+
+if PossibleAmount >= cups:
+    print("Yes, I can make that amount of coffee(and even " + str(PossibleAmount - cups) + " more than that)")
+else:
+    print("No, I can make only " + str(PossibleAmount) + " cups of coffee")
+    
