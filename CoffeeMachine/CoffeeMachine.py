aa56b49087481031b147b9d2fc0dce1724a92836
Lam4ek <d.i.iljyusha@student.khai.edu>
2021-10-31 16:08:25 +0200

    CoffeeMachine 4-st stage

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -1,28 +1,68 @@
-print("Starting to make a coffee")
-print("Grinding coffee beans")
-print("Boiling water")
-print("Mixing boiled water with crushed coffee beans")
-print("Pouring coffee into the cup")
-print("Pouring some milk into the cup")
-print("Coffee is ready!")
-water = 200
-milk = 50
-CoffeeBeans = 15
+water = 400
+milk = 540
+CoffeeBeans = 120
+DisposableCups = 9
+money = 550
+print("The coffee machine has:")
+print(str(water) + " of water")
+print(str(milk) + " of milk")
+print(str(CoffeeBeans) + " of coffee beans")
+print(str(DisposableCups) + " of disposable cups")
+print(str(money) + " of money")
 
-# print("For 25 cups of coffee you will need:")
-# print(str(cups * water) + "ml of water") 
-# print(str(cups * milk) + "ml of milk")
-# print(str(cups * CoffeeBeans) + "g of coffee beans")
+while True:
+    a = input("Write action (buy, fill, take):") 
+    if a == "buy":
+        print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+        type_of_coffee = input()
+        if type_of_coffee == "1":
+            water -= 250
+            CoffeeBeans -= 16
+            DisposableCups -= 1
+            money += 4
+            print("The coffee machine has:")
+            print(str(water) + " of water")
+            print(str(milk) + " of milk")
+            print(str(CoffeeBeans) + " of coffee beans")
+            print(str(DisposableCups) + " of disposable cups")
+            print(str(money) + " of money")
+        if type_of_coffee == "2":
+            water -= 350
+            milk -=75
+            CoffeeBeans -= 20
+            DisposableCups -=1
+            money += 7
+            print("The coffee machine has:")
+            print(str(water) + " of water")
+            print(str(milk) + " of milk")
+            print(str(CoffeeBeans) + " of coffee beans")
+            print(str(DisposableCups) + " of disposable cups")
+            print(str(money) + " of money")
+        if type_of_coffee == "3":
+            water -= 200
+            milk -= 100
+            CoffeeBeans -= 12
+            DisposableCups -=1
+            money += 6
+            print("The coffee machine has:")
+            print(str(water) + " of water")
+            print(str(milk) + " of milk")
+            print(str(CoffeeBeans) + " of coffee beans")
+            print(str(DisposableCups) + " of disposable cups")
+            print(str(money) + " of money")
+    if a == "fill":
+        water += int(input("Write how many ml of water you want to add:"))
+        milk += int(input("Write how many ml of milk you want to add:"))
+        CoffeeBeans += int(input("Write how many grams of coffee beans you want to add:"))
+        DisposableCups += int(input("Write how many disposable coffee cups you want to add:"))
+    if a == "take":
+        print("I gave you " + str(money))
+        money -= money
+        print("The coffee machine has:")
+        print(str(water) + " of water")
+        print(str(milk) + " of milk")
+        print(str(CoffeeBeans) + " of coffee beans")
+        print(str(DisposableCups) + " of disposable cups")
+        print(str(money) + " of money")
 
-amount_of_water = int(input("Write how many ml of water the coffee machine has:"))
-amout_of_milk = int(input("Write how many ml of milk the coffee machine has:"))
-amout_of_CoffeeBeans = int(input("Write how many grams of coffee beans the coffee machine has:"))
-cups = int(input("Write how many cups of coffee you will need:"))
-
-PossibleAmount = min(amount_of_water //  water, amout_of_milk // milk, amout_of_CoffeeBeans // CoffeeBeans)
-
-if PossibleAmount >= cups:
-    print("Yes, I can make that amount of coffee(and even " + str(PossibleAmount - cups) + " more than that)")
-else:
-    print("No, I can make only " + str(PossibleAmount) + " cups of coffee")
-    
+            
