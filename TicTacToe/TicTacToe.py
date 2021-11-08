6ba519f6710282663cd377bf491b94fecb082b64
Lam4ek <d.i.iljyusha@student.khai.edu>
2021-11-08 22:09:48 +0200

    TicTacToe 2-nd stage

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,1 +1,12 @@
-print("X O X \nO X O \nX X O")
+board = ("---------")
+def PlayingField():
+    for i in range(len(board)):
+        if i == 0 or i == 3 or i == 6:
+            print("|", end='')
+        if i == 2 or i == 5 or i == 8:
+            print(board[i] + "|")
+        else:
+            print(board[i], end=' ')
+PlayingField()
+board = list[:8](input("Enter cells: "))
+PlayingField()
