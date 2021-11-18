489cde26e8bc193f013dc502a38887e7b00b1479
Lam4ek <d.i.iljyusha@student.khai.edu>
2021-11-18 20:48:41 +0200

    TicTacToe 5-rd stage

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,21 +1,57 @@
-symbols = list("---------")
-def PlayingField():
-    for i in range(len(symbols)):
-        if i == 0 or i == 3 or i == 6:
-            print("|", end='')
-        if i == 2 or i == 5 or i == 8:
-            print(symbols[i] + "|")
-        else:
-            print(symbols[i], end=' ')
-PlayingField()
-def check_result(symbols):
-    win_coord = [(0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6)]
-    for each in win_coord:
-        if symbols[each[0]] == symbols[each[1]] == symbols[each[2] != '-']:
-            print(str(symbols[each[0]]) + " win" )
-            break
+board = list(range(1,10))
 
-if __name__ == "__main__":
-    symbols = list(input("Enter cells: "))
-    PlayingField()
-    check_result(symbols)
+def draw_board(board):
+   print("-" * 13)
+   for i in range(3):
+      print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
+      print("-" * 13)
+
+def take_input(player_token):
+   valid = False
+   while not valid:
+      player_answer = input("Where do we put it " + player_token+"? ")
+      try:
+         player_answer = int(player_answer)
+      except:
+         print("Incorrect input. Are you sure you enter a number?")
+         continue
+      if player_answer >= 1 and player_answer <= 9:
+         if(str(board[player_answer-1]) not in "XO"):
+            board[player_answer-1] = player_token
+            valid = True
+         else:
+            print("This position is already occupied!")
+      else:
+        print("Incorrect input. Enter numbers from 1 to 9.")
+
+def check_win(board):
+   win_coord = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
+   for each in win_coord:
+       if board[each[0]] == board[each[1]] == board[each[2]]:
+          return board[each[0]]
+   return False
+
+def main(board):
+    counter = 0
+    win = False
+    while not win:
+        draw_board(board)
+        if counter % 2 == 0:
+            take_input("X")
+        else:
+            take_input("O")
+        counter += 1
+        if counter > 4:
+           tmp = check_win(board)
+           if tmp:
+              print(tmp, "Win!")
+              win = True
+              break
+        if counter == 9:
+            print("Draw!")
+            break
+    draw_board(board)
+main(board)
+
+
+input("Press Enter to exit!") 
