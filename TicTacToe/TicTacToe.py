f5515c412b828cd11d0d7317678d823840499a52
Lam4ek <d.i.iljyusha@student.khai.edu>
2021-11-18 16:01:06 +0200

    TicTacToe 3-rd dtage

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,12 +1,21 @@
-board = ("---------")
+symbols = list("---------")
 def PlayingField():
-    for i in range(len(board)):
+    for i in range(len(symbols)):
         if i == 0 or i == 3 or i == 6:
             print("|", end='')
         if i == 2 or i == 5 or i == 8:
-            print(board[i] + "|")
+            print(symbols[i] + "|")
         else:
-            print(board[i], end=' ')
+            print(symbols[i], end=' ')
 PlayingField()
-board = list[:8](input("Enter cells: "))
-PlayingField()
+def check_result(symbols):
+    win_coord = [(0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6)]
+    for each in win_coord:
+        if symbols[each[0]] == symbols[each[1]] == symbols[each[2] != '-']:
+            print(str(symbols[each[0]]) + " win" )
+            break
+
+if __name__ == "__main__":
+    symbols = list(input("Enter cells: "))
+    PlayingField()
+    check_result(symbols)
