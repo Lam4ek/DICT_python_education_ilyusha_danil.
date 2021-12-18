5d40b1850bd54ae83bf1ca644977fd45de4ab2d9
Lam4ek <d.i.iljyusha@student.khai.edu>
2021-12-18 16:09:48 +0200

    DinnerParty 2-nd stage

--- DinnerParty/DinnerParty.py
+++ DinnerParty/DinnerParty.py
@@ -1,11 +1,12 @@
 number_of_member=int(input("Enter the number of friends joining (including you):"))
-def getDict():
+if number_of_member<=0:
+    print("No one is joining for the party")
+else:
+    amount=int(input("Enter the total amount :"))
+    a=round(amount/number_of_member)
     member={}
     while len(member)<number_of_member:
         print('Enter name: ')
         name=input()
-        member[name]=0
+        member[name]=a
     print(member)
- 
-getDict()
-
