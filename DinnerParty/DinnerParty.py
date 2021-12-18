215e6b2751932ba713ee144ab5b5ea60add06de2
Lam4ek <d.i.iljyusha@student.khai.edu>
2021-12-18 17:41:58 +0200

    DinnerParty 4-st stage

--- DinnerParty/DinnerParty.py
+++ DinnerParty/DinnerParty.py
@@ -1,12 +1,30 @@
-number_of_member=int(input("Enter the number of friends joining (including you):"))
-if number_of_member<=0:
-    print("No one is joining for the party")
-else:
-    amount=int(input("Enter the total amount :"))
-    a=round(amount/number_of_member)
-    member={}
-    while len(member)<number_of_member:
-        print('Enter name: ')
-        name=input()
-        member[name]=a
+import random
+def unluck(member, amount, number_of_member):
+    names=list(member.keys())
+    for i in names:
+        member[i] = amount/(number_of_member)  
     print(member)
+
+def luck(member, amount, number_of_member):
+    names=list(member.keys())
+    for i in names:
+        member[i] = amount/(number_of_member-1)   
+    random_key = random.choice(list(member.keys()))
+    member[random_key] = 0
+    print(random_key + " is the lucky one!")
+    print(member)
+member={}
+number_of_member=int(input("Enter the number of friends joining (including you):"))
+if number_of_member>0:
+    amount=int(input("Enter the total amount :"))
+    while len(member)<number_of_member:
+            name=input('Enter name: ')
+            member[name]=""
+    print('Do you want to use the "Who is lucky?" feature? Write Yes/No:')
+    choise=input()
+    if choise=="yes":
+        luck(member, amount, number_of_member)
+    else:
+        unluck(member, amount, number_of_member)
+else:
+    print("No one is joining for the party")
