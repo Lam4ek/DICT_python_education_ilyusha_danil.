3b299ab246f91e886b43f387bdc17da1e06bdd51
Lam4ek <d.i.iljyusha@student.khai.edu>
2022-06-03 19:25:23 +0100

    arithmetic_test stage 2

--- arithmetic_test/arithmetic_test.py
+++ arithmetic_test/arithmetic_test.py
@@ -1,18 +1,30 @@
 import random
-a = random.choice([2,3,4,5,6,7,8,9])
-b = random.choice([2,3,4,5,6,7,8,9])
-operation = ['+','-','*']
-c = random.choice(operation)
-guess = (str(a) + c + str(b))
-print (guess)
-answer = int(input())
-if c == '+':
-    guess = a+b
-if c == '-':
-    guess = a-b
-if c == '*':
-    guess = a*b
-if answer == guess:
-    print('right!')
-else:
-    print('wrong!')
+attempts = 0
+score = 0
+i = True
+while i == True:
+    a = random.choice([2,3,4,5,6,7,8,9])
+    b = random.choice([2,3,4,5,6,7,8,9])
+    operation = ['+','-','*']
+    c = random.choice(operation)
+    print (str(a) + c + str(b))
+    answer = (input())
+    if answer.isnumeric(): 
+        if c == '+':
+            guess = a+b
+        if c == '-':
+            guess = a-b
+        if c == '*':
+            guess = a*b
+        if int(answer) == guess:
+            print('right!')
+            attempts = attempts+1
+            score=score+1
+        else:
+            print('wrong!')
+            attempts=attempts+1
+        if attempts == 5:
+            i=False
+    else:
+        print("incorect fornat")
+print("Your mark is " + str(score) + " /5.")
